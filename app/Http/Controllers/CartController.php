aecb98c0e34b0cd6402b75d54566a7c8a887f8aa
winsonallan <83772638+winsonallan@users.noreply.github.com>
2023-01-16 19:53:21 +0700

    controller headers

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,6 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ShopCart;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Models\UserType;
 
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ShopCart;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Models\UserType;
 
 class ProductController extends Controller
 {
